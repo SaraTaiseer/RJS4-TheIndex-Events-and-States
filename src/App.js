f8edaa7fd17330605f0f727288ca7a06a9754aa8
sarataiseer <sarataiseer97@gmail.com>
2020-03-12 11:59:39 +0200

    RJS4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{Component} from "react";
 
 // Data
 import authors from "./data";
@@ -6,8 +6,31 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail"
 
-function App() {
+class App extends Component {
+  state ={
+    currentAuthor:null 
+  };
+  back = () => this.setState({currentAuthor:null});
+  selectAuthor = author => this.setState({ currentAuthor:author });
+
+  render(){
+    if (this.state.currentAuthor){
+      return (
+        <div id="app" className="container-fluid">
+          <div className="row">
+            <div className="col-2">
+              <Sidebar back={this.back} />
+            </div>
+            <div className="content col-10">
+              <AuthorDetail author={this.state.currentAuthor}/>
+            </div>
+          </div>
+        </div>
+      );
+
+    }
   return (
     <div id="app" className="container-fluid">
       <div className="row">
@@ -15,11 +38,13 @@ function App() {
           <Sidebar />
         </div>
         <div className="content col-10">
-          <AuthorList authors={authors} />
+          <AuthorList authors={authors}  selectAuthor={this.selectAuthor}/>
+       
         </div>
       </div>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/AuthorDetail.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+function AuthorDetail(props) {
+    const author=props.author;
+    const card = props.author.books.map(book =>(
+        <tr>
+                <td>{book.title}</td>
+                <td>{author.first_name} {author.last_name}</td>
+                <td>
+                    <button className="btn" style={{backgroundColor:book.color}}/>
+                </td>
+            </tr>
+    ) );
+  return (
+    <div className="author col-xs-10">
+    <div>
+        <h3>{author.first_name} {author.last_name}</h3>
+        <img src={author.imageUrl} className="img-thumbnail" alt={`${author.first_name} ${author.last_name}`}/>
+    </div>
+    <table className='mt-3 table'>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Authors</th>
+                <th>Color</th>
+            </tr>
+        </thead>
+        <tbody>
+            {card}
+        </tbody>
+    </table>
+</div>
+  );
+};
+
+export default AuthorDetail;
